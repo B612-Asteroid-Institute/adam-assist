afe99f159ca56877db594bd41384977163905017
Alec Koumjian <akoumjian@gmail.com>
2023-12-05 12:53:38 -0800

    fix times

--- src/adam_assist/__init__.py
+++ src/adam_assist/__init__.py
@@ -1,6 +1,7 @@
 import hashlib
 import os
 import pathlib
+from ctypes import c_uint32
 from importlib.resources import files
 from typing import Dict, List, Optional, Tuple
 
@@ -8,18 +9,15 @@ import assist
 import numpy as np
 import pyarrow as pa
 import pyarrow.compute as pc
-import rebound
 import quivr as qv
+import rebound
 import urllib3
-from adam_core.coordinates import CartesianCoordinates, Origin, transform_coordinates
+from adam_core.coordinates import (CartesianCoordinates, Origin,
+                                   transform_coordinates)
 from adam_core.coordinates.origin import OriginCodes
-from adam_core.propagator.propagator import (
-    EphemerisType,
-    ObserverType,
-    OrbitType,
-    Propagator,
-    TimestampType,
-)
+from adam_core.propagator.propagator import (EphemerisType, ObserverType,
+                                             OrbitType, Propagator,
+                                             TimestampType)
 from adam_core.time import Timestamp
 from quivr.concat import concatenate
 
@@ -28,9 +26,9 @@ DATA_DIR = os.getenv("ASSIST_DATA_DIR", "~/.adam_assist_data")
 
 class EarthImpacts(qv.Table):
     orbit_id = qv.StringColumn()
-    time = Timestamp.as_column()
     # Distance from earth center in km
     distance = qv.Float64Column()
+    coordinates = CartesianCoordinates.as_column()
 
 
 def download_jpl_ephemeris_files(data_dir: str = DATA_DIR):
@@ -70,10 +68,10 @@ def initialize_assist(data_dir: str = DATA_DIR) -> assist.Extras:
     return sim, ephem
 
 
-def uint32_hash(s):
+def uint32_hash(s) -> c_uint32:
     sha256_result = hashlib.sha256(s.encode()).digest()
     # Get the first 4 bytes of the SHA256 hash to obtain a uint32 value.
-    return int.from_bytes(sha256_result[:4], byteorder="big") % (2**32)
+    return c_uint32(int.from_bytes(sha256_result[:4], byteorder="big"))
 
 
 def hash_orbit_ids_to_uint32(
@@ -86,7 +84,10 @@ def hash_orbit_ids_to_uint32(
     Here we attempt to generate uint32 hashes for each and return the mapping as well.
     """
     hashes = [uint32_hash(o) for o in orbit_ids]
-    mapping = {hashes[i]: orbit_ids[i] for i in range(len(orbit_ids))}
+    # Because uint32 is an unhashable type, 
+    # we use a dict mapping from uint32 to orbit id string
+    mapping = {hashes[i].value: orbit_ids[i] for i in range(len(orbit_ids))}
+    
     return mapping, hashes
 
 
@@ -101,7 +102,7 @@ class ASSISTPropagator(Propagator):
         # Assert that the time for each orbit definition is the same for the simulator to work
         assert len(pc.unique(orbits.coordinates.time.mjd())) == 1
 
-        sim, ephem = initialize_assist()
+        # sim, ephem = initialize_assist()
 
         # The coordinate frame is the equatorial International Celestial Reference Frame (ICRF). 
         # This is also the native coordinate system for the JPL binary files.
@@ -118,9 +119,16 @@ class ASSISTPropagator(Propagator):
         coords = coords.set_column("time", input_orbit_times)
         orbits = orbits.set_column("coordinates", coords)
 
+        root_dir = pathlib.Path(DATA_DIR).expanduser()
+        ephem = assist.Ephem(
+            root_dir.joinpath("linux_p1550p2650.440"),
+            root_dir.joinpath("sb441-n16.bsp"),
+        )
+        sim = rebound.Simulation()
+
         # Set the simulation time, relative to the jd_ref
-        start_tbd_time = orbits.coordinates.time.jd().to_numpy()[0] - ephem.jd_ref
-        sim.t = start_tbd_time
+        start_tdb_time = orbits.coordinates.time.jd().to_numpy()[0] - ephem.jd_ref
+        sim.t = start_tdb_time
 
         output_type = type(orbits)
 
@@ -142,10 +150,14 @@ class ASSISTPropagator(Propagator):
                 hash=uint_orbit_ids[i],
             )
 
+        ax = assist.Extras(sim, ephem)
+
         # Prepare the times as jd - jd_ref
+        integrator_times = times.rescale("tdb").jd()
         integrator_times = pc.subtract(
-            times.rescale("tdb").jd(), ephem.jd_ref
-        ).to_numpy()
+            integrator_times, ephem.jd_ref
+        )
+        integrator_times = integrator_times.to_numpy()
 
         results = None
 
@@ -162,7 +174,6 @@ class ASSISTPropagator(Propagator):
 
             # Retrieve original orbit it from hash
             orbit_ids = [orbit_id_mapping[h] for h in orbit_id_hashes]
-
             time_step_results = output_type.from_kwargs(
                 coordinates=CartesianCoordinates.from_kwargs(
                     x=step_xyzvxvyvz[:, 0],
@@ -171,7 +182,7 @@ class ASSISTPropagator(Propagator):
                     vx=step_xyzvxvyvz[:, 3],
                     vy=step_xyzvxvyvz[:, 4],
                     vz=step_xyzvxvyvz[:, 5],
-                    time=Timestamp.from_jd(pa.repeat(sim.t + ephem.jd_ref, sim.N)),
+                    time=Timestamp.from_jd(pa.repeat(sim.t + ephem.jd_ref, sim.N), scale="tdb"),
                     origin=Origin.from_kwargs(
                         code=pa.repeat(
                             "SOLAR_SYSTEM_BARYCENTER",
@@ -199,19 +210,56 @@ class ASSISTPropagator(Propagator):
 
         impacts = sim._extras_ref.get_impacts()
         earth_impacts = None
-        for impact in impacts:
-            orbit_id = orbit_id_mapping[impact["hash"]]
+        for i, impact in enumerate(impacts):
+            orbit_id = orbit_id_mapping.get(impact["hash"], f"unknown-{i}")
+            time = Timestamp.from_jd([impact["time"]], scale="tdb")
             if earth_impacts is None:
+                coordinates=CartesianCoordinates.from_kwargs(
+                        x=[impact["x"]],
+                        y=[impact["y"]],
+                        z=[impact["z"]],
+                        vx=[impact["vx"]],
+                        vy=[impact["vy"]],
+                        vz=[impact["vz"]],
+                        time=time,
+                        origin=Origin.from_kwargs(
+                            code=["SOLAR_SYSTEM_BARYCENTER"],
+                        ),
+                        frame="equatorial",
+                    )
+                coordinates = transform_coordinates(
+                    coordinates,
+                    origin_out=OriginCodes.SUN,
+                    frame_out="ecliptic",
+                )
                 earth_impacts = EarthImpacts.from_kwargs(
                     orbit_id=[orbit_id],
-                    time=Timestamp.from_jd([impact["time"] + ephem.jd_ref], scale="tdb"),
                     distance=[impact["distance"]],
+                    coordinates=coordinates,
                 )
             else:
+                coordinates=CartesianCoordinates.from_kwargs(
+                        x=[impact["x"]],
+                        y=[impact["y"]],
+                        z=[impact["z"]],
+                        vx=[impact["vx"]],
+                        vy=[impact["vy"]],
+                        vz=[impact["vz"]],
+                        time=time,
+                        origin=Origin.from_kwargs(
+                            code=["SOLAR_SYSTEM_BARYCENTER"],
+                        ),
+                        frame="equatorial",
+                    )
+                coordinates = transform_coordinates(
+                    coordinates,
+                    origin_out=OriginCodes.SUN,
+                    frame_out="ecliptic",
+                )
                 earth_impacts = qv.concatenate(earth_impacts, EarthImpacts.from_kwargs(
                     orbit_id=[orbit_id],
-                    time=Timestamp.from_jd([impact["time"] + ephem.jd_ref], scale="tdb"),
                     distance=[impact["distance"]],
+                    coordinates=coordinates,
                 ))
 
         return results, earth_impacts
