07d8d763b5534a19d371be0f9c9f593ac6d25efa
Alec Koumjian <akoumjian@gmail.com>
2024-05-07 10:59:18 -0400

    New adam-assist propagator and impact detector as subpackage

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,19 +1,42 @@
+import time
+
 import numpy as np
-from adam_assist import ASSISTPropagator, download_jpl_ephemeris_files
+import pytest
+from adam_core.dynamics.impacts import calculate_impacts
+from adam_core.orbits import Orbits
 from adam_core.orbits.query import query_sbdb
-from adam_core.propagator.pyoorb import PYOORB
 from adam_core.time import Timestamp
 
+from adam_core.propagator.adam_assist import (
+    ASSISTPropagator,
+    download_jpl_ephemeris_files,
+)
 
-def test_assist_propagator():
-    download_jpl_ephemeris_files()
-    initial_time = Timestamp.from_mjd([60000.0], scale="tdb")
-    times = initial_time.from_mjd(initial_time.mjd() + np.arange(0, 100))
-    orbits = query_sbdb(["EDLU"])
+# Contains a likely impactor with ~60% chance of impact in 30 days
+IMPACTOR_FILE_PATH = "tests/data/I00007_orbit.parquet"
+
+
+def test_detect_impacts():
+    impactors = Orbits.from_parquet(IMPACTOR_FILE_PATH)[0]
     propagator = ASSISTPropagator()
-    assist_propagated_orbits = propagator.propagate_orbits(orbits, times)
+    variants, impacts = propagator.detect_impacts(impactors)
+    return variants, impacts
 
-    pyoorb = PYOORB()
-    pyoorb_propagated_orbits = pyoorb.propagate_orbits(orbits, times)
 
-    return assist_propagated_orbits, pyoorb_propagated_orbits
+@pytest.mark.benchmark
+@pytest.mark.parametrize("processes", [1, None])
+def test_calculate_impacts_benchmark(benchmark, processes):
+    download_jpl_ephemeris_files()
+    impactor = Orbits.from_parquet(IMPACTOR_FILE_PATH)[0]
+    propagator = ASSISTPropagator()
+    variants, impacts = benchmark(
+        calculate_impacts,
+        impactor,
+        60,
+        propagator,
+        num_samples=1000,
+        processes=processes,
+    )
+    assert len(variants) == 1000, "Should have 1000 variants"
+    assert len(impacts) > 500, "Should have at least 500 impactors"
+    assert len(impacts) < 700, "Should have less than 700 impactors"
