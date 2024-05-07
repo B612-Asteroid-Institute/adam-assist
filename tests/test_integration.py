9c0fbcd419664d4e8e9fa18cffcb3bc9ee68469a
Alec Koumjian <akoumjian@gmail.com>
2024-05-07 12:14:15 -0400

    try new imports

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,26 +1,14 @@
-import time
-
-import numpy as np
 import pytest
 from adam_core.dynamics.impacts import calculate_impacts
 from adam_core.orbits import Orbits
-from adam_core.orbits.query import query_sbdb
-from adam_core.time import Timestamp
 
-from ..src.adam_core.propagator.adam_assist import (
+from src.adam_core.propagator.adam_assist import (
     ASSISTPropagator,
     download_jpl_ephemeris_files,
 )
 
 # Contains a likely impactor with ~60% chance of impact in 30 days
 IMPACTOR_FILE_PATH = "tests/data/I00007_orbit.parquet"
-
-
-def test_detect_impacts():
-    impactors = Orbits.from_parquet(IMPACTOR_FILE_PATH)[0]
-    propagator = ASSISTPropagator()
-    variants, impacts = propagator.detect_impacts(impactors)
-    return variants, impacts
 
 
 @pytest.mark.benchmark
