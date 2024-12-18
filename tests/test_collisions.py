283ee107ae3bbb5c7f3d165a9c305cd19b86851d
Kathleen Kiker <kathleen@b612foundation.orgm>
2024-12-18 12:17:23 -0800

    Removing detect_impacts and EarthImpact class

--- tests/test_collisions.py
+++ tests/test_collisions.py
@@ -7,6 +7,7 @@ IMPACTOR_FILE_PATH_100 = "tests/data/I00008_orbit.parquet"
 # Contains a likely impactor with 0% chance of impact in 30 days
 IMPACTOR_FILE_PATH_0 = "tests/data/I00009_orbit.parquet"
 
+
 def test_detect_collisions():
     orbits = Orbits.from_parquet(IMPACTOR_FILE_PATH_100)[0]
     propagator = ASSISTPropagator()
@@ -16,7 +17,9 @@ def test_detect_collisions():
         collision_distance=[7000],
         stopping_condition=[True],
     )
-    results, collisions = propagator._detect_collisions(orbits, 60, collision_conditions)
+    results, collisions = propagator._detect_collisions(
+        orbits, 60, collision_conditions
+    )
 
     assert len(collisions) == 1
     assert collisions.distance.to_numpy()[0] <= 7000
@@ -26,6 +29,8 @@ def test_detect_collisions():
         collision_distance=[10000, 7000],
         stopping_condition=[False, True],
     )
-    results, collisions = propagator._detect_collisions(orbits, 60, collision_conditions)
+    results, collisions = propagator._detect_collisions(
+        orbits, 60, collision_conditions
+    )
 
     assert len(collisions) > 1
